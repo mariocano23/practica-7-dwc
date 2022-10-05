d01492c34fb61181f1ee53c6c957f6206c3c611c
mariocano23 <mariocano.alu@iespacomolla.es>
2022-10-05 19:02:23 +0200

    ejer1

---
+++ src/Ejercicio1/Contenedor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Cabezera from './Cabecera';
+import Peliculero from './Peliculero';
+import PiePagina from './PiePagina';
+
+function Contenedor(props){
+    return(
+        <React.Fragment>
+            <wrapper>
+                <Cabezera descri="Página de Películas"></Cabezera>
+                <Peliculero titulo="Titanic" direccion="James Cameron, Pamela Easley, Jon Landou" cartel="https://r3.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic4.abc.es%2Fmedia%2Fpeliculas%2F000%2F044%2F428%2Ftitanic-1.jpg&nuevoancho=620&medio=abc">
+      Jack es un joven artista que gana un pasaje para viajar a América en el Titanic, el transatlántico más grande y seguro jamás construido.
+    </Peliculero>
+            </wrapper>
+            <PiePagina autor="Mario Cano"></PiePagina>
+        </React.Fragment>
+    )
+}
+
+export default Contenedor;
